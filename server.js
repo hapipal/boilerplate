629aaa286ee564ab5511f3b168cd48939b5cb43c
devinivy <devin@bigroomstudios.com>
2015-08-10 09:36:05 -0400

    Upgrade to hapi8, lint for hapi style.

--- server.js
+++ server.js
@@ -1,50 +1,59 @@
 var Path = require('path');
 var Hapi = require('hapi');
+var Glue = require('glue');
 var Config = require('./config');
 
 
-Config.server.boilerplateApi.uri = (Config.server.boilerplateApi.tls ? 'https://' : 'http://') + Config.server.boilerplateApi.host + ':' + Config.server.boilerplateApi.port;
-
+Config.server.boilerplateApi.uri = (Config.server.boilerplateApi.tls ? 'https://' : 'http://') +
+                                    Config.server.boilerplateApi.host + ':' +
+                                    Config.server.boilerplateApi.port;
 
 var manifest = {
-    
-    pack: {
+
+    server: {
         app: {
-            config: Config,
+            config: Config
         }
     },
-    
-    servers: [
+
+    connections: [
         {
             host: Config.server.boilerplateApi.host,
             port: Config.server.boilerplateApi.port,
-            options: {
-                labels: 'boilerplate-api',
+            labels: 'boilerplate-api',
+            routes: {
                 cors: true
             }
-        },
+        }
     ],
-    
+
     plugins: {
-        
+
         // General porpoise
         './dogwater':   Config.dogwater,
         './poop':       Config.poop,
-        
+
         // Server-specific
         '../lib': [{ select: 'boilerplate-api' }]
-        
+
     }
-    
+
 };
 
 module.exports = manifest;
 
-// If this is being required, return the manifest.  Otherwise, start the pack.
+// If this is being required, return the manifest.  Otherwise, start the server.
 if (!module.parent) {
-    Hapi.Pack.compose(manifest, { relativeTo: Path.join(__dirname, 'node_modules') }, function (err, pack) {
-        
-        pack.start();
-    }); 
-}
+    Glue.compose(manifest, { relativeTo: Path.join(__dirname, 'node_modules') }, function (err, server) {
 
+        if (err) {
+
+            throw err;
+        }
+
+        server.start(function () {
+
+            console.log('Started on ' + Config.server.boilerplateApi.uri);
+        });
+    });
+}
