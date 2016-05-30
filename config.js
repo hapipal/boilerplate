b7ef170973ad78409df1e59ab6d05ac463a9c0e7
William S. Woodruff <williamswoodruff@gmail.com>
2016-05-29 20:39:58 -0400

    add haute-couture to boilerplate

--- config.js
+++ config.js
@@ -12,7 +12,8 @@ module.exports = {
         boilerplateApi: {
             host: '0.0.0.0',
             port: process.env.PORT || 3000
-        }
+        },
+        instance: null
     },
 
     dogwater: {
