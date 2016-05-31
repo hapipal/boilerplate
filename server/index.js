3f17aa4af1156845550ed567db5dd14252c979ae
Devin Ivy <devin@bigroomstudios.com>
2016-05-31 11:50:22 -0400

    Pull server into tests with labbable, separate server a bit, simplify manifest

---
+++ server/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Hoek = require('hoek');
+const Glue = require('glue');
+const Labbable = require('labbable');
+const Manifest = require('./manifest');
+
+const labbable = module.exports = new Labbable();
+
+Glue.compose(Manifest, { relativeTo: __dirname }, (err, server) => {
+
+    Hoek.assert(!err, err);
+
+    // Pass server along to labbable
+    labbable.using(server);
+
+    server.initialize((err) => {
+
+        Hoek.assert(!err, err);
+
+        // No need to start server if this is being required (i.e. for testing)
+        if (module.parent) {
+            return;
+        }
+
+        server.start((err) => {
+
+            Hoek.assert(!err, err);
+
+            console.log(`Boilerplate API Started at ${server.info.uri}`);
+        });
+    });
+});
