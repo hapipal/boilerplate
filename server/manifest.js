9a1e5cba95859037574630dc5c78646c9ab69d10
Devin Ivy <devin@bigroomstudios.com>
2018-01-08 21:00:44 -0500

    (flavor) templated-site v2.0.0

--- server/manifest.js
+++ server/manifest.js
@@ -23,7 +23,9 @@ module.exports = new Confidence.Store({
         plugins: [
             {
                 plugin: '../lib', // Main plugin
-                options: {}
+                options: {
+                    developmentMode: (process.env.NODE_ENV !== 'production')
+                }
             }
         ]
     }
