08a0993af231b38a8024e9307ec4e09ee5e702a7
Matt Boutet <matt@bigroomstudios.com>
2018-03-23 11:23:19 -0400

    (flavor) objection v2.1.0

--- server/manifest.js
+++ server/manifest.js
@@ -24,6 +24,29 @@ module.exports = new Confidence.Store({
             {
                 plugin: '../lib', // Main plugin
                 options: {}
+            },
+            {
+                plugin: 'schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'sqlite3',
+                            useNullAsDefault: true,         // Suggested for sqlite3
+                            pool: {
+                                idleTimeoutMillis: Infinity // Handles knex v0.12/0.13 misconfiguration when using sqlite3 (tgriesser/knex#1701)
+                            },
+                            connection: {
+                                filename: ':memory:'
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
             }
         ]
     }
