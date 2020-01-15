b7d73146374b4cbb0419d125b6c0b532ea2b0eca
Devin Ivy <devin@bigroomstudios.com>
2020-01-15 00:23:28 -0500

    (flavor) custom-swagger v2.4.0

---
+++ server/plugins/swagger/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Package = require('../../../package.json');
+
+module.exports = {
+    name: 'app-swagger',
+    async register(server) {
+
+        await server.register([
+            Inert,
+            Vision,
+            {
+                plugin: HapiSwagger,
+                options: {
+                    templates: __dirname,
+                    validatorUrl: null,
+                    info: {
+                        version: Package.version
+                    }
+                }
+            }
+        ]);
+    }
+};
