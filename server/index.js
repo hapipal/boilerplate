f4f8901dad65c9cc250d8944317a071ffb09bc77
Devin Ivy <devin@bigroomstudios.com>
2021-03-27 23:51:48 -0400

    Update dependencies to @hapipal scoped modules

--- server/index.js
+++ server/index.js
@@ -3,27 +3,24 @@
 const Glue = require('@hapi/glue');
 const Manifest = require('./manifest');
 
-exports.deployment = async (start) => {
+exports.deployment = async ({ start } = {}) => {
 
-    const manifest = Manifest.get('/');
+    const manifest = Manifest.get('/', process.env);
     const server = await Glue.compose(manifest, { relativeTo: __dirname });
 
     await server.initialize();
 
-    if (!start) {
-        return server;
+    if (start) {
+        await server.start();
+        server.log(['start'], `Server started at ${server.info.uri}`);
     }
-
-    await server.start();
-
-    console.log(`Server started at ${server.info.uri}`);
 
     return server;
 };
 
-if (!module.parent) {
+if (require.main === module) {
 
-    exports.deployment(true);
+    exports.deployment({ start: true });
 
     process.on('unhandledRejection', (err) => {
 
