2a479ff637f1b150585b009edb76b4a4055eb4cd
Devin Ivy <devin@bigroomstudios.com>
2021-04-11 23:24:17 -0400

    (flavor) swagger v3.0.0

--- server/manifest.js
+++ server/manifest.js
@@ -34,6 +34,9 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
+                plugin: './plugins/swagger'
+            },
+            {
                 plugin: {
                     $filter: 'NODE_ENV',
                     $default: '@hapipal/hpal-debug',
